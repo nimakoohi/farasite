fbb6f35fcf3c52f2c8ded35c9ec29d527c7b142a
nimakoohi <nimak93@proton.me>
2024-12-14 20:21:19 +0330

    Add Home.html!!!!

--- farasite_project/farasite/views.py
+++ farasite_project/farasite/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Farasite!")
+    return render(request, 'home.html')
